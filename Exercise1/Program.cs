e2309cefcb21cc4a5fdfed0dd7e762c4bbb9dd7a
Tatiana <kudrina_tatyana@mail.ru>
2023-04-11 21:06:32 +0200

    Added readme file

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа
+//в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+int naturals(int n)
+{
+    if(n == 1) 
+    {
+    Console.Write("1");
+    return 1; 
+    }
+    else 
+    {
+        Console.Write($"{n},");
+        return naturals(n - 1);
+    }
+  }
+naturals(5);
+
+
+
+  
--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных 
+//элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+int sum(int n)
+{
+    if (n<0) return 0;
+    else return n+sum(n-1);
+    }
+Console.Write(sum(50));
