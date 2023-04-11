25b9c0868f4a808b019ab22e33cfa8f6e1fc8bc0
Tatiana <kudrina_tatyana@mail.ru>
2023-04-11 21:23:24 +0200

    Third code

---
+++ Exercise3/Program.cs
@@ -0,0 +1,10 @@
+﻿int Ackermann(int m, int n)
+    {
+        if (m == 0)
+            return n + 1;
+        else if (n == 0)
+            return Ackermann(m - 1, 1);
+        else
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+Console.WriteLine(Ackermann(2,3));
